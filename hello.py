1fd0171fd5b3ac05305bc49318df2ceb2e1c2959
Jandi07 <torjandi07@gmail.com>
2026-07-11 17:24:56 +0100

    Add interactive greeting script

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+print(f"Hello, {name}!")
+
+age = int(input("How old are you? "))
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
